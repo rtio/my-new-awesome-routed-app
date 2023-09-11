601a6e391e6baaed6ba753c9920876cca0a9ec61
Rafael Meneses <meneses.tio@gmail.com>
2023-09-11 20:07:12 -0300

    monday

---
+++ src/Root.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Menu from "./components/Menu";
+
+export default function Root() {
+    return (
+        <>
+            <Menu />
+            <Outlet />
+        </>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root'
+import Index from './pages/Index';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
